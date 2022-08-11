4bf0771605cb444c8af2aa842c1703c9b4353b5d
LynnLee93 <linglynn1221@gmail.com>
2022-08-11 22:33:32 +0800

    add upload img and send button

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,9 @@
 import React from "react";
-import TextField from "@mui/material/TextField";
 import { makeStyles } from "@mui/styles";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 
 const useStyles = makeStyles({
   root: {},
@@ -29,35 +32,47 @@ const useStyles = makeStyles({
     justifyContent: "space-between",
     marginBottom: "20px",
   },
+  btn: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
 });
 
 function Form() {
   const styles = useStyles();
   return (
-    <div>
-      <div className={styles.centered}>
-        <div className={styles.title}>
-          <h3>Simple Form</h3>
+    <div className={styles.centered}>
+      <div className={styles.title}>
+        <h3>Simple Form</h3>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.name}>
+          <TextField required id="outlined-required" label="First Name" />
+          <TextField required id="outlined-required" label="Last Name" />
         </div>
-        <div className={styles.container}>
-          <div className={styles.name}>
-            <TextField required id="outlined-required" label="First Name" />
-            <TextField required id="outlined-required" label="Last Name" />
-          </div>
-          <div className={styles.desc}>
-            <TextField
-              id="outlined-multiline-static"
-              label="Description"
-              multiline
-              rows={4}
-              fullWidth
-            />
-          </div>
-
-          <div className={styles.email}>
-            <TextField id="outlined-required" label="Email" fullWidth />
-          </div>
+        <div className={styles.desc}>
+          <TextField
+            id="outlined-multiline-static"
+            label="Description"
+            multiline
+            rows={4}
+            fullWidth
+          />
         </div>
+        <div className={styles.email}>
+          <TextField id="outlined-required" label="Email" fullWidth />
+        </div>
+      </div>
+      <div className={styles.btn}>
+        <IconButton
+          color="primary"
+          aria-label="upload picture"
+          component="label"
+        >
+          <input hidden accept="image/*" type="file" />
+          <AddPhotoAlternateIcon />
+        </IconButton>
+        <Button variant="contained">Send</Button>
       </div>
     </div>
   );
